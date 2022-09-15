526a5ada7795abb521ab1d0efcc9d69e1afb15ca
yoshi1203 <yoshivn1203@gmail.com>
2022-09-15 22:37:21 +0700

    finish hero

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <h2>A warm welcome!</h2>
+      <p>
+        Bootstrap utility classes are used to create this jumbotron since the old
+        component has been removed from the framework. Why create custom CSS when you can
+        use utilities?
+      </p>
+      <button>Call to action</button>
+    </div>
+  );
+};
+
+export default Hero;
